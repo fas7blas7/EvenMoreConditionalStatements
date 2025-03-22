8dab8e97fe3dfd841ad1da0c4b9609f5da0635f6
Martin Kirov <fas7blas7@gmail.com>
2025-03-22 17:42:10 +0200

    Adding ProductOfThreeNumbers task

---
+++ 6-ProductOfThreeNumbers/ProductOfThreeNumbers.cs
@@ -0,0 +1,42 @@
+﻿namespace _6_ProductOfThreeNumbers
+{
+    internal class ProductOfThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            int negativeNumberCount = 0;
+
+            if (number1 == 0 || number2 == 0 || number3 == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else
+            {
+                if (number1 < 0)
+                {
+                    negativeNumberCount++;
+                }
+                if (number2 < 0)
+                {
+                    negativeNumberCount++;
+                }
+                if (number3 < 0)
+                {
+                    negativeNumberCount++;
+                }
+                if (negativeNumberCount % 2 == 1)
+                {
+                    Console.WriteLine("negative");
+                }
+                else
+                {
+                    Console.WriteLine("positive");
+                }
+            }
+        }
+    }
+}
