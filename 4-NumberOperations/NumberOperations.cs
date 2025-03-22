e01e875de8e37222105cc809619f0660fb36a933
Martin Kirov <fas7blas7@gmail.com>
2025-03-22 17:41:13 +0200

    Adding NumberOperations task

---
+++ 4-NumberOperations/NumberOperations.cs
@@ -0,0 +1,31 @@
+﻿namespace _4_NumberOperations
+{
+    internal class NumberOperations
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+
+            if(@operator == "+")
+            {
+                Console.WriteLine($"{number} {@operator} {number2} = {number + number2:F2}");
+            }
+            else if (@operator == "-")
+            {
+                Console.WriteLine($"{number} {@operator} {number2} = {number - number2:F2}");
+            }
+            else if (@operator == "*")
+            {
+                Console.WriteLine($"{number} {@operator} {number2} = {number * number2:F2}");
+            }
+            else if (@operator == "/")
+            {                
+                {
+                    Console.WriteLine($"{number} / {number2} = {number / number2:F2}");
+                }
+            }
+        }
+    }
+}
