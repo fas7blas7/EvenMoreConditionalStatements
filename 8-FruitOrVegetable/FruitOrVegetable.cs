ca9d403486abcd1a98fb02404a4884a795a36f73
Martin Kirov <fas7blas7@gmail.com>
2025-03-24 22:21:14 +0200

    Adding FruitOrVegetable task

---
+++ 8-FruitOrVegetable/FruitOrVegetable.cs
@@ -0,0 +1,23 @@
+﻿namespace _8_FruitOrVegetable
+{
+    internal class FruitOrVegetable
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+
+            if (product is "banana" or "apple" or "kiwi" or "cherry" or "lemon")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (product is "cucumber" or "pepper" or "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
