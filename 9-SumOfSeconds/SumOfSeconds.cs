a3a025d1849a82eb15dc6badcbf2378f1a3644bf
Martin Kirov <fas7blas7@gmail.com>
2025-03-25 21:24:55 +0200

    Adding SumOfSeconds task

---
+++ 9-SumOfSeconds/SumOfSeconds.cs
@@ -0,0 +1,19 @@
+﻿namespace _9_SumOfSeconds
+{
+    internal class SumOfSeconds
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int totalTimeInSecods = firstTime + secondTime + thirdTime;
+
+            int minutes = totalTimeInSecods / 60;
+            int seconds = totalTimeInSecods % 60;
+
+            Console.WriteLine($"{minutes}:{seconds:D2}");
+        }
+    }
+}
